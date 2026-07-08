25e7905e655df4a0952a5a500145210f5ef76368
lucasvinbr <lucasvinbr@gmail.com>
2026-07-08 16:41:05 -0300

    Sync with orig origin

--- cpack.cmake
+++ cpack.cmake
@@ -3,9 +3,9 @@
 # It sets the package name, version, and other metadata required for packaging.
 # It also specifies the files to include in the package and the installation directories.
 # The package will be created in the build directory and can be used to distribute the application.
-set(CPACK_PACKAGE_CONTACT "JÃ¶rg Macedo da Costa Zopes")
+set(CPACK_PACKAGE_CONTACT "Jörg Macedo da Costa Zopes")
 set(CPACK_PACKAGE_NAME "ClamAV-GUI")
-set(CPACK_PACKAGE_VERSION "1.4.0")
+set(CPACK_PACKAGE_VERSION "1.4.1")
 set(CPACK_PACKAGE_FILE_NAME "ClamAV-GUI-${CPACK_PACKAGE_VERSION}-${CMAKE_SYSTEM_PROCESSOR}")
 
 set(CPACK_APPIMAGE_GUESS_UPDATE_INFORMATION ON)
@@ -16,7 +16,7 @@ set(CPACK_ARCHIVING_COMPONENT_INSTALL ON)
 set(CPACK_RPM_COMPONENT_INSTALL ON)
 set(CPACK_DEB_COMPONENT_INSTALL ON)
 
-set(CPACK_DEBIAN_PACKAGE_MAINTAINER "JÃ¶rg Macedo da Costa Zopes")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Jörg Macedo da Costa Zopes")
 set(CPACK_DEBIAN_PACKAGE_SECTION "utils")
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "clamav, clamav-daemon, clamav-freshclam")
 set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "ClamAV GUI for Linux")
